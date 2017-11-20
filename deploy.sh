bb34520e6421fd1af23412529e3b6e5d541bed0d
Timothy Moore <mtimothy983@gmail.com>
2017-11-20 12:14:19 -0500

    Touch includes/common.php + update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-  echo "Not running as root"
-  exit
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 
+  exit 1
 fi
 
+rm -rf ~/website_tmp
 git clone --depth=1 --branch=master https://github.com/Tjstretchalot/USLBot-Site ~/website_tmp
 rsync -avh ~/website_tmp/html /var/www/html
 rsync -avh ~/website_tmp/includes /var/www/includes
+rm -rf ~/website_tmp
