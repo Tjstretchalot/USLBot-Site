533a4b39f4b6a39e387cea03abea6547f6f0ae50
Timothy Moore <mtimothy983@gmail.com>
2017-11-21 15:51:01 -0500

    Prettier tooltip on login page

--- html/login.php
+++ html/login.php
@@ -56,6 +56,10 @@ include 'pagestart.php';
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
     <script type="text/javascript">
+      $(function () {
+	  $('[data-toggle="tooltip"]').tooltip();
+      });
+
       $("#login-form").on('submit', function(e) {
 	e.preventDefault();
 	console.log('login form submitted');
