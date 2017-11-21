8b5859d0fdfd3ec4367511bcd8b8260b18292cf7
Timothy Moore <mtimothy983@gmail.com>
2017-11-21 14:32:11 -0500

    Fix another missing parens

--- includes/database/site_sessions.php
+++ includes/database/site_sessions.php
@@ -50,7 +50,7 @@
 
     public static function fetch_by_session_id($sql_conn, $session_id) {
       $err_prefix = 'SiteSessionMapping#fetch_by_session_id';
-      check_db_error($sql_conn, $err_prefix, $stmt = $sql_conn->prepare('SELECT * FROM site_sessions WHERE session_id=?');
+      check_db_error($sql_conn, $err_prefix, $stmt = $sql_conn->prepare('SELECT * FROM site_sessions WHERE session_id=?'));
       check_db_error($sql_conn, $err_prefix, $stmt->bind_param('s', $session_id));
       check_db_error($sql_conn, $err_prefix, $stmt->execute());
       check_db_error($sql_conn, $err_prefix, $res = $stmt->get_result());
