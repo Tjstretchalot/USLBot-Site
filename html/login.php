fc578eb2ef551fde262c6958b0e675b613938e82
Timothy Moore <mtimothy983@gmail.com>
2017-11-22 12:40:06 -0500

    use full jquery for login page

--- html/login.php
+++ html/login.php
@@ -15,7 +15,7 @@ include 'pagestart.php';
     <?php include 'navigation.php'; ?>
     <div class="pl-1 pr-1">
       <div class="container mt-5">
-        <div class="container-fluid mt-5 mb-5 ml-5 mr-5" id="statusText"></div>
+        <div class="container-fluid pt-5 pb-5 pl-5 pr-5" id="statusText" hidden></div>
 	<form id="login-form">
 	  <div class="form-group row">
 	    <input type="text" class="form-control" id="username" aria-label="Username" placeholder="Username">
@@ -55,7 +55,7 @@ include 'pagestart.php';
       </div>
     </div>
     <?php include 'footer.php'; ?>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
