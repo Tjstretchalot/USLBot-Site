0cbdda234c7d1a2ee3680c3616d675233f443726
Timothy Moore <mtimothy983@gmail.com>
2017-11-22 13:07:23 -0500

    Fix missing closing small tag in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <!-- Begin footer -->
 <footer class="footer">
   <div class="container-fluid text-right">
-    <small class="text-muted mr-5">&copy;<?= date("Y") ?> Timothy Moore</span>
+    <small class="text-muted mr-5">&copy;<?= date("Y") ?> Timothy Moore</small>
   </div>
 </footer>
 <!-- End footer -->
