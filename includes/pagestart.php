33c9da71388be740b0f807ece60a8e4503bbf918
Timothy Moore <mtimothy983@gmail.com>
2017-11-28 14:17:38 -0500

    consistent name for session cookie

--- includes/pagestart.php
+++ includes/pagestart.php
@@ -6,12 +6,12 @@
   $conn = create_db_connection();
 
   $logged_in_person = null;
-  if(isset($_COOKIE['usl_session_id'])) {
-    $session = SiteSessionMapping::fetch_by_session_id($conn, $_COOKIE['usl_session_id']);
+  if(isset($_COOKIE['session_id'])) {
+    $session = SiteSessionMapping::fetch_by_session_id($conn, $_COOKIE['session_id']);
 
     if($session === null || $session->expires_at <= time()) {
-      unset($_COOKIE['usl_session_id']);
-      setcookie('usl_session_id', '', time() - 3600, '/');
+      unset($_COOKIE['session_id']);
+      setcookie('session_id', '', time() - 3600, '/');
     }else {
       $logged_in_person = PersonMapping::fetch_by_id($session->person_id); 
     }
