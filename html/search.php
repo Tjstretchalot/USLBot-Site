3bdd11b9c6cc217671c56a540ecd26825cbe34d1
Timothy Moore <mtimothy983@gmail.com>
2017-12-04 17:08:23 -0500

    add some animation and fix NaN time

--- html/search.php
+++ html/search.php
@@ -15,42 +15,42 @@ include 'pagestart.php';
     <div class="container mt-5">
       <form id="search-form">
         <div class="form-group row">
-	  <input type="text" class="form-control" id="search_for" aria-label="Who to search for" placeholder="Who?">
-	</div>
-	<div class="form-group row justify-content-around">
-	  <div class="form-check col-auto">
-	    <label class="form-check-label">
-	      <input class="form-check-input" type="checkbox" id="scammer-checkbox" checked> #scammer
-	    </label>
-	  </div>
-	  <div class="form-check col-auto">
-	    <label class="form-check-label">
-	      <input class="form-check-input" type="checkbox" id="sketchy-checkbox" checked> #sketchy
-	    </label>
-	  </div>
-	  <div class="form-check col-auto">
-	    <label class="form-check-label">
-	      <input class="form-check-input" type="checkbox" id="troll-checkbox" checked> #troll
-	    </label>
-	  </div>
-	</div>
-	<input type="submit" class="sr-only" />
+          <input type="text" class="form-control" id="search_for" aria-label="Who to search for" placeholder="Who?">
+        </div>
+        <div class="form-group row justify-content-around">
+          <div class="form-check col-auto">
+            <label class="form-check-label">
+              <input class="form-check-input" type="checkbox" id="scammer-checkbox" checked> #scammer
+            </label>
+          </div>
+          <div class="form-check col-auto">
+            <label class="form-check-label">
+              <input class="form-check-input" type="checkbox" id="sketchy-checkbox" checked> #sketchy
+            </label>
+          </div>
+          <div class="form-check col-auto">
+            <label class="form-check-label">
+              <input class="form-check-input" type="checkbox" id="troll-checkbox" checked> #troll
+            </label>
+          </div>
+        </div>
+        <input type="submit" class="sr-only" />
       </form>
       <hr />
       <div id="output-not-grandfathered">
         <table id="not-gfather-table" class="table">
-	  <thead>
-	    <tr>
-	      <th>Kind</th>
-	      <th>Subreddit</th>
-	      <th data-breakpoints="md">Description</th>
-	      <th data-breakpoints="sm">Details</th>
-	      <th data-breakpoints="sm">Time</th>
-	    </tr>
-	  </thead>
-	  <tbody id="not-gfather-tbody">
-	  </tbody>
-	</table>
+          <thead>
+            <tr>
+              <th>Kind</th>
+              <th>Subreddit</th>
+              <th data-breakpoints="md">Description</th>
+              <th data-breakpoints="sm">Details</th>
+              <th data-breakpoints="sm">Time</th>
+            </tr>
+          </thead>
+          <tbody id="not-gfather-tbody">
+          </tbody>
+        </table>
       </div>
     </div>
     <?php include 'footer.php'; ?>
@@ -66,61 +66,64 @@ include 'pagestart.php';
       });
 
       $("#search-form").on('submit', function(e) {
-	e.preventDefault();
-	
-	var hashtags = [];
-	if($("#scammer-checkbox").is(":checked")) {
-	  hashtags.push("#scammer");
-	}
-	if($("#sketchy-checkbox").is(":checked")) {
-	  hashtags.push("#sketchy");
-	}
-	if($("#troll-checkbox").is(":checked")) {
-	  hashtags.push("#troll");
-	}
+        e.preventDefault();
+        
+        var hashtags = [];
+        if($("#scammer-checkbox").is(":checked")) {
+          hashtags.push("#scammer");
+        }
+        if($("#sketchy-checkbox").is(":checked")) {
+          hashtags.push("#sketchy");
+        }
+        if($("#troll-checkbox").is(":checked")) {
+          hashtags.push("#troll");
+        }
 
-	$("#search_for").attr('disabled', true);
+        $("#search_for").attr('disabled', true);
 
-	$.get("/api/query.php", { query: $("#search_for").val(), hashtags: hashtags.join(','), format: 2 }, function(data, stat) {
-	  if(!data.data.grandfathered) {
-	    var table = $("#not-gfather-table");
-	    var tbody = $("#not-gfather-tbody");
-	    tbody.empty();
+        $.get("/api/query.php", { query: $("#search_for").val(), hashtags: hashtags.join(','), format: 2 }, function(data, stat) {
+          if(!data.data.grandfathered) {
+            var table = $("#not-gfather-table");
+            var tbody = $("#not-gfather-tbody");
+            tbody.slideUp('fast', function() {
+              tbody.empty();
 
-	    var new_html = "";
-	    for(var i = 0; i < data.data.history.length; i++) {
-	      var ele = data.data.history[i];
-	      new_html += "<tr>";
-	      new_html += "<td>" + ele.kind + "</td>";
-	      new_html += "<td>" + ele.subreddit + "</td>";
-	      new_html += "<td>" + ele.description + "</td>";
-	      new_html += "<td>" + ele.details + "</td>";
-	      new_html += "<td>" + $.timeago(new Date(data.time * 1000)) + "</td>";
-	      new_html += "</tr>";
-	    }
-	    tbody.html(new_html);
+              var new_html = "";
+              for(var i = 0; i < data.data.history.length; i++) {
+                var ele = data.data.history[i];
+                new_html += "<tr>";
+                new_html += "<td>" + ele.kind + "</td>";
+                new_html += "<td>" + ele.subreddit + "</td>";
+                new_html += "<td>" + ele.description + "</td>";
+                new_html += "<td>" + ele.details + "</td>";
+                new_html += "<td>" + $.timeago(new Date(ele.time * 1000)) + "</td>";
+                new_html += "</tr>";
+              }
+              tbody.html(new_html);
+              table.footable();
+              tbody.slideDown('fast', function() { 
+                $("#search_for").removeAttr('disabled');
+              });
+            });
+          }
+        }).fail(function(xhr) {
+          console.log(xhr.responseJSON);
 
-            table.footable();
-	  }
-	  $("#search_for").removeAttr('disabled');
-	}).fail(function(xhr) {
-	  console.log(xhr.responseJSON);
-
-	  var json_resp = xhr.responseJSON;
-	  var err_type = json_resp.error_type;
-	  var err_mess = json_resp.error_message;
-	  console.log(err_type + ": " + err_mess);
+          var json_resp = xhr.responseJSON;
+          var err_type = json_resp.error_type;
+          var err_mess = json_resp.error_message;
+          console.log(err_type + ": " + err_mess);
 
           statusText.fadeOut('fast', function() {
-	    statusText.removeClass("alert-success").removeClass("alert-info");
-	    statusText.addClass("alert-danger");
-	    statusText.html("<span class=\"glyphicon glyphicon-remove\"></span> " + err_mess);
-	    $("#search_for").removeAttr('disabled');
-	    statusText.fadeIn('fast');
-	  });
-	});
-	
-	
+            statusText.removeClass("alert-success").removeClass("alert-info");
+            statusText.addClass("alert-danger");
+            statusText.html("<span class=\"glyphicon glyphicon-remove\"></span> " + err_mess);
+            $("#search_for").removeAttr('disabled');
+            statusText.fadeIn('fast');
+          });
+        });
+        
+        
       });
     </script>
   </body>
