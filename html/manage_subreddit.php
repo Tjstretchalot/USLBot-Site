95b2ba286f31c6b6f690ceb7d131c3257935b418
Timothy Moore <mtimothy984@gmail.com>
2018-12-26 12:08:21 -0800

    Remove unnecessary import

--- html/manage_subreddit.php
+++ html/manage_subreddit.php
@@ -143,7 +143,6 @@ if($auth_level < $MODERATOR_PERMISSION) {
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-    <script src="js/jquery.basictable.min.js"></script>
     <script src="js/status_text_utils.js"></script>
     <script src="js/moment.js"></script>
     <script src="js/moment-timezone.js"></script>
