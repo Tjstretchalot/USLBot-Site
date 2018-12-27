fca5c3a32800f69e9e9f2d7b13226a670d88d83b
Timothy Moore <mtimothy984@gmail.com>
2018-12-27 11:31:19 -0800

    Fix importing popper before bootstrap

--- html/manage_subreddit.php
+++ html/manage_subreddit.php
@@ -212,8 +212,8 @@ if($auth_level < $MODERATOR_PERMISSION) {
 
     <?php include 'footer.php'; ?>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/esm/popper.min.js" integrity="sha256-T0gPN+ySsI9ixTd/1ciLl2gjdLJLfECKvkQjJn98lOs=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/esm/popper.min.js" integrity="sha256-T0gPN+ySsI9ixTd/1ciLl2gjdLJLfECKvkQjJn98lOs=" crossorigin="anonymous"></script>
     <script src="js/status_text_utils.js"></script>
     <script src="js/moment.js"></script>
     <script src="js/moment-timezone.js"></script>
